b54be9f4282c760a136beddbacee749a32633d7d
niyatimalhotra <malhotra.niyati@gmail.com>
2021-01-12 14:08:37 -0500

    Add lecture 1 script and assignment 1 files to repository

---
+++ Assignments/Assignment1.r
@@ -0,0 +1,85 @@
+################################################################################
+##
+## [ PROJ ] Assignment1
+## [ FILE ] Assignment1-YOURUNI.r
+## [ AUTH ] < YOUR NAME >
+## [ DATE ] < SUBMISSION DATE >
+##
+################################################################################
+
+## Assignment 1: Getting familiar with R & RStudio
+
+## Include the code to answer the following
+## Use comments to organize and provide your responses to questions
+## Submit only this R script (Assignment1.r)
+
+
+## -----------------------------------------------------------------------------
+## 0. create a new R project called Assignment1
+## -----------------------------------------------------------------------------
+
+# This is for your internal project management, do not submit your .rproj file
+
+
+
+## -----------------------------------------------------------------------------
+## 1. load the gapminder package (to access the gapminder data)
+## -----------------------------------------------------------------------------
+
+
+
+
+## -----------------------------------------------------------------------------
+## 2. a) use the str() function to give an overview of the gapminder data frame
+##        (found in the gapminder package)
+##    b) how many observations and variables are there?
+## -----------------------------------------------------------------------------
+
+#a
+
+#b 
+
+
+
+## -----------------------------------------------------------------------------
+## 3. a) what is the average gdpPercap across all observations in the dataset
+##    b) use ?gapminder to view the documentation & find the units for gdpPercap
+##    c) how would you intepret this mean, what is it the mean of?
+##       HINT: try executing View(gapminder)
+## -----------------------------------------------------------------------------
+
+#a
+
+#b. 
+
+#c
+
+
+## -----------------------------------------------------------------------------
+## 4. a) plot year vs. gdpPercap
+##    b) what does the plot say about economic growth over time (1 sentence)
+##    c) describe what a better visualization might look like 
+##       (don't worry about how to do this in R... we'll get there!)
+##       TIP: try typing your answer using ENTER to fit in the margins,
+##            then select the text & use CTRL+SHIFT+C to comment multiple lines
+## -----------------------------------------------------------------------------
+
+#a
+
+#b
+
+#c
+
+
+
+## -----------------------------------------------------------------------------
+## 5. create a barplot showing the number of observations in each continent
+##    a) start by using the table function with continent as its argument,
+##       assign the results of this function to a new object
+##    b) next pass the object you created as an argument to the barplot function
+## -----------------------------------------------------------------------------
+
+#a
+
+#b
+
--- Lectures/Lecture1/Lecture1-inclass.r
+++ Lectures/Lecture1/Lecture1-inclass.r
@@ -3,7 +3,7 @@
 ## [ PROJ ] Lecture1-inclass: Getting familiar with RStudio
 ## [ FILE ] Lecture1-inclass.r
 ## [ AUTH ] INSTRUCTOR FILE 
-## [ INIT ] 9/8/2020
+## [ INIT ] Jan 19, 2021
 ##
 ################################################################################
 
@@ -17,114 +17,112 @@
 ## -----------------------------------------------------------------------------
 
 #get working directory
-getwd()
+  getwd()
+
 
 ## -----------------------------------------------------------------------------
 ## 2. install and load the gapminder package 
 ## -----------------------------------------------------------------------------
 
 #first we have to install the package
-install.packages("gapminder")
+  install.packages("gapminder")
 
-#we can see the list of packages under "Packages" on the bottom right panel of RStudio
+#we can see the list of packages under in the bottom right panel of RStudio
 
-#to load this package, type the following:
-library(gapminder)
-#another way to load is to click the checkbox beside the package name under "Packages"
+#load this package
+  library(gapminder)
+    #can also load by clicking the checkbox beside package name under "Packages"
+
 
 ## -----------------------------------------------------------------------------
-## 3. Use base R functions to inspect a dataframe included w/ gapminder package 
+## 3. Use base R functions to inspect a data frame included w/ gapminder package 
 ##    (this exercise is based on STAT545 by Jenny Bryan)
 ## -----------------------------------------------------------------------------
 
-gapminder <- gapminder
-
 #let's use some functions inspect the structure of the gapminder object
-str(gapminder) #str (structure) is the function, gapminder is the argument
-#notice that each variable has its own data type (Factor, int, num)
+  str(gapminder) #str (structure) is the function, gapminder is the argument
+    #notice that each variable has its own data type (Factor, int, num)
 
-#how we access built-in help files/documentation... also try Google 
-#(e.g. Stack Exchange or Stack Overflow)
-?str
+#access built-in help files/documentation (also use Google)
+  ?str
 
 #the function class() tells us what class(es) an object is assigned to... more next week
-class(gapminder)
+  class(gapminder)
 
 #head is another function that shows us the first parts of an object
-head(gapminder)
-
-#note the presence of an optional argument n, which is the number of rows you want to see
-?head
+  head(gapminder)
+  ?head
+    #note the optional argument n, which is the # of rows you want to display
 
 #let's try to see the first 10 rows
-head(gapminder, n = 10)
+  head(gapminder, n = 10)
 
 #in order to view the full data frame, simply type View(data_frame_name) on the Console
-#in this case, it is View(gapminder)
+  View(gapminder)
+
 
 ## -----------------------------------------------------------------------------
 ## 4. Use some base R functions to perform some very basic exploratory analysis
 ## -----------------------------------------------------------------------------
 
 #let's use some base R functions to understand the basic structure of the data frame
-names(gapminder) #get the column names; can also be colnames(gapminder)
-dim(gapminder) #get the number of rows and columns
-ncol(gapminder) #get the number of columns (alternatively, dim(gapminder)[2])
-nrow(gapminder) #get the number of rows (alternatively, dim(gapminder)[1])
-names(gapminder)[3] #get the name of the third column
+  names(gapminder) #get the column names
+  dim(gapminder) #get the number of rows and columns
+  ncol(gapminder) #get the number of columns (alternatively, dim(gapminder)[2])
+  nrow(gapminder) #get the number of rows (alternatively, dim(gapminder)[1])
+  names(gapminder)[3] #get the name of the third column
 
 #note that indexing in R starts from 1, NOT 0
 
 #we can assign the result of a function if we want to refer back to it:
-num_of_vars <- ncol(gapminder) #assign the results of nrow to a new object called num_of_vars
-
-num_of_vars #note that we need to type the object name again to view it
+  num_of_vars <- ncol(gapminder) #assign the results of nrow to a new object called num_of_vars
+  num_of_vars #note that we need to type the object name again to view it
 
 #get summary statistics for each variable
-summary(gapminder)
-#notice that the stats shown depend on the type of each column/variable
-#for Factor, it lists how many times each level appears
-#for int or num, it lists the first, second, third quartiles as well as min, max and mean
+  summary(gapminder)
+  #notice that the stats shown depend on the type of each column/variable
+  #for Factor, it lists how many times each level appears
+  #for int or num, it lists the first, second, third quartiles as well as min, max and mean
 
 ## let's plot the relationship of year (x) vs lifeExp (y) using base R
-plot(lifeExp ~ year, gapminder) #the tilde (~) operator sets up a 'formula'
+  plot(lifeExp ~ year, gapminder) #the tilde (~) operator sets up a "formula"
 
 #alternative 1
-plot((y = lifeExp) ~ (x = year), data = gapminder)
+  plot((y = lifeExp) ~ (x = year), data = gapminder)
 
 #alternative 2
-plot(gapminder$year, gapminder$lifeExp)
-
-plot(x = gapminder$year, y = gapminder$lifeExp, main = "Scatter plot of life expectancy and year",
-     xlab = "Year", ylab = "Life Expectancy (in years)")
+  plot(gapminder$year, gapminder$lifeExp)
 
 #let's look at the documentation
-?plot
+  ?plot
 
 #let's apply some functions to specific variables (i.e. columns of data) 
 #start with a continuous numeric variable (object type 'int')
 #let's see how R uses $ to refer to a variable in a dataframe (subsetting)
-head(gapminder$lifeExp)
-summary(gapminder$lifeExp)
-hist(gapminder$lifeExp)
+  head(gapminder$lifeExp)
+  summary(gapminder$lifeExp)
+  hist(gapminder$lifeExp)
 
 #next let's look at a factor variable
 
 #what is a factor? let's look at some base R functions to figure it out
-str(gapminder)
-class(gapminder$continent)
-summary(gapminder$continent)
-levels(gapminder$continent) #returns the levels of a factor vector
-nlevels(gapminder$continent) #returns the number of levels of a factor vector
-#in this case, it's 5 since we have Africa, Americas, Asia, Europe and Oceania
+  str(gapminder)
+  class(gapminder$continent)
+  summary(gapminder$continent)
+  levels(gapminder$continent) #returns the levels of a factor vector
+  nlevels(gapminder$continent) #returns the number of levels of a factor vector
+  #in this case, it's 5 since we have Africa, Americas, Asia, Europe and Oceania
 
 #Note: a factor is just R's way of recognizing a categorical variable!
 
 #here's one way to run a frequency table to check the number of obs per year
-table(gapminder$year)
+  table(gapminder$year)
 
 #let's assign this frequency table to a new object called freq_byyear
-freq_byyear <- table(gapminder$year)
+  freq_byyear <- table(gapminder$year)
 
 #then pass this object (a column of data!) as an argument to the barplot function
-barplot(freq_byyear)
+  barplot(freq_byyear)
+
+
+  
