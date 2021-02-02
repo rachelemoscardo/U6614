e6428925b36f7101249a05fff1651966f607543c
niyatimalhotra <malhotra.niyati@gmail.com>
2021-02-02 14:30:56 -0500

    Push lecture 4 r script to repo

---
+++ Lectures/Lecture4/Lecture4-inclass-part1.R
@@ -0,0 +1,335 @@
+################################################################################
+##
+## [ PROJ ] Lecture 4: Subway Fare Evasion Arrests and Racial Bias (part 1)
+## [ FILE ] Lecture4-inclass.r
+## [ AUTH ] < YOUR NAME >
+## [ INIT ] < Feb. 2nd, 2021 >
+##
+################################################################################
+
+## POLICY QUESTION FOR THIS WEEK & NEXT:
+## Police can stop and ticket or arrest people for subway fare evasion. 
+## Is NYPD enforcement of subway fare evasion enforcement in Brooklyn racist?
+
+
+## --------------------------------------
+## 1. load libraries and check directory
+## --------------------------------------
+
+#install.packages("weights")
+#install.packages("lmtest")
+#install.packages("sandwich")
+#install.packages("knitr")
+
+library(tidyverse)
+library(weights)
+library(lmtest)
+library(sandwich)
+library(knitr)
+
+getwd()
+
+
+## -----------------------------------------------------------------------------
+## 2. Aggregating to subway station-level arrest totals
+##
+##  a. load cleaned/appended arrest microdata (arrests_all.csv) w/all strings as factors
+##
+##  b. aggregate microdata to station-level observations w/the following info:
+##      - st_id, loc2, arrests_all (=arrest count)
+##      - store results as new data frame st_arrests
+##
+##  c. plot histogram of arrests and briefly describe distribution of arrests across stations
+## -----------------------------------------------------------------------------
+
+#2a.
+  arrests_all <- read.csv("arrests_all.csv", 
+                         stringsAsFactors = TRUE,
+                          na.strings = c("")) #specify string values to read in as NA (here blanks)
+#2b.
+  st_arrests <- arrests_all %>% 
+    group_by(st_id, loc2) %>% 
+    summarise(arrests_all = n() ) %>% 
+    arrange(desc(arrests_all))
+
+  #could use count() in place of group_by() & summarise() - shorter but less flexible
+  arrests_all %>% 
+    count(st_id, loc2) %>% 
+    arrange(desc(n))
+  
+#2c.
+  ggplot(data = st_arrests, aes(x = arrests_all)) + geom_histogram()
+
+
+## -----------------------------------------------------------------------------
+## 3. joining ridership and neighborhood demographics to arrest data
+##
+##  a. import other station-level datasets and inspect
+##  
+##  b. join both files to st_arrests and inspect results (store new df as st_joined),
+##      - inspect results of join and describe any issues
+##      - drop unnecessary columns from the ridership data
+##      - group st_joined by st_id and mta_name
+##      - only display ungrouped version of st_joined for compactness
+##        - i.e. don't display all of your inspection commands in your knit submission
+##
+##  c. print top 10 stations by arrest counts
+##      - only display st_id, mta_name, arrests_all, shareblack, povrt_all_2016
+## -----------------------------------------------------------------------------
+
+#3a. 
+  st_poverty <- read.csv("station_povdataclean_2016.csv", 
+                         stringsAsFactors = TRUE)
+  
+  #background on poverty data (from 2016 American Community Survey): 
+  #  Source: https://usa.ipums.org/usa/acs.shtml
+  #  each observation represents a "subway station area", defined as follows:
+  #   - all census tracts w/geometric center within .5km of a station (see map)
+  #  st_id is the unique identifier for each subway station area
+  #  povrt_all_2016: % of adults in subway station area living below federal poverty level
+  #  shareblack: share of adults in subway station area who identify as Black
+  #  nblack: dummy variable = 1 if shareblack >= 50%, 0 otherwise
+  
+  
+  st_ridership <- read.csv("Subway Ridership by Station - BK.csv", 
+                           stringsAsFactors = TRUE)
+  #background on MTA ridership data:
+  #  Source: http://web.mta.info/nyct/facts/ridership/ridership_sub_annual.htm
+  #  each observation is a subway station w/a unique identifier (st_id)
+  #  includes annual # of MetroCard swipes at each station for 2011-16
+
+str(st_poverty)
+str(st_ridership)
+
+
+#3b.
+  #a vector of columns we don't need to keep
+    drop_vars <- c("swipes2011", "swipes2012", "swipes2013", "swipes2014", "swipes2015")
+    
+  st_joined <- inner_join(st_poverty, st_ridership) %>%
+      inner_join(st_arrests) %>% 
+      select(!drop_vars) %>% 
+      group_by(st_id, mta_name) 
+
+  #inspect
+    str(st_joined) #too long, not informative to show in your rmd submission
+    summary(st_joined)
+      #Note: 157 obs in joined df w/no NAs (except some missing demographics) 
+      #inner join worked as intended!
+  
+  #display structure of ungrouped data frame to avoid lengthy output listing every group
+    st_joined %>% ungroup() %>% str()
+
+    
+#3c.
+  st_joined %>% 
+    arrange(desc(arrests_all)) %>% 
+    select(st_id, mta_name, arrests_all, shareblack, povrt_all_2016) %>% 
+    head(n = 10) 
+
+
+## -----------------------------------------------------------------------------
+## 4. Explore relationship between arrest intensity & poverty rates across stations
+##
+##  a. compute variables for arrest intensity and other explanatory variables
+##     - exclude coney island station from the analysis sample
+##     - create new variable measuring fare evasion arrest intensity:
+##       - arrperswipe_2016 = arrests per 100,000 swipes
+##     - create new dummy variable indicating high poverty station area:
+##       - highpov = 1 if pov rate is > median pov rate across stations
+##     - create new dummy for majority Black station areas (shareblack > .5)
+##     - coerce new dummies into factors w/category labels
+##     - assign results to new df called stations
+##     - validate results 
+##     - display top 10 stations by arrest intensity using kable() in knitr package
+##       - only show st_id, mta_name, arrests_all and new columns
+##
+##  b. investigate arrests intensity vs poverty rates 
+##     - plot arrperswipe vs povrt_all_2016
+##     - should we weight stations by # of MetroCard swipes?
+##     - investigate linear and quadratic model fit 
+##  
+##  c. report diff in mean arrest intensity between high/low pov areas
+##     - weight observations by swipes for difference in group means
+##     - is this difference statistically significant?
+## ---------------------------------------------------------------------------
+
+#4a.
+  stations <- st_joined %>%
+    filter(st_id != 66) %>%
+    mutate(arrperswipe = round(arrests_all / (swipes2016 / 100000), 2),
+           highpov = as.numeric(povrt_all_2016 > median(st_joined$povrt_all_2016)),
+           nblack = as.numeric(shareblack > .5), 
+           highpov = factor(highpov, levels = c(0,1), 
+                            labels = c("Not high poverty", "High poverty")),
+           nblack  = factor(nblack, levels = c(0,1), 
+                            labels = c("Majority non-Black", "Majority Black")),
+           shareblack = round(shareblack, 2),
+           povrt_all_2016 = round(povrt_all_2016, 2)) 
+  
+      #note we can directly test conditions as a logical comparison
+      #then we convert logical results into numeric (i.e. a dummy variable)
+      #we also continued and converted to factors
+  
+  #some inspection and validation
+  
+    #check if nblack recoding worked as intended
+      stations %>% 
+        group_by(nblack) %>% 
+        summarise(min(shareblack), mean(shareblack), max(shareblack))
+
+    #examine joint distribution of highpov and black
+      table(stations$highpov, stations$nblack)
+      
+  
+  #display top 10 stations by arrest intensity (show st_id, mta_name, arrests_all and new variables)
+    stations_top10 <- stations %>% 
+      arrange(desc(arrperswipe)) %>% 
+      select(st_id, mta_name, arrperswipe, arrests_all, shareblack, povrt_all_2016, highpov, nblack) %>% 
+      head(n = 10)
+    kable(stations_top10) #kable offers better table formatting
+  
+
+  
+#4b. 
+  ggplot(stations, #specify dataframe to use
+         aes(x = povrt_all_2016, y = arrperswipe, weights = swipes2016)) + #specify columns to use
+    geom_point() + #specify plot geometry
+    ggtitle('Scatterplot of arrest rate vs. poverty rate') + #add title
+    labs(x = 'poverty rate', y = 'arrest rate') #change axis labels
+
+  
+  #add linear prediction line to scatter plot
+
+
+  #add quadratic prediction line to scatter plot
+  
+
+  
+#4c. calculate and test difference in means between high/low poverty stations
+    
+
+
+    
+
+## -----------------------------------------------------------------------------
+## 5. How does neighborhood racial composition mediate the relationship between poverty and arrest intensity
+##    - examine relationship between arrest intensity & poverty by Black vs non-Black station area (nblack)
+##
+##    a. difference in means table: arrests per swipe by highpov vs nblack
+##        - weighted by station swipes
+##        - could difference in arrest intensity be explained by differences in povrt?
+##
+##    b. scatterplot of arrperswipe vs povrt_all by nblack
+##       - add linear fit
+##       - add quadratic regression fit
+##
+##    c. which model do you prefer, linear or quadratic?
+##      - why? be clear about your logic and if applicable cite statistical evidence to support your decision
+##
+##    d. interpret your preferred regression specification (carefully!)
+## -----------------------------------------------------------------------------
+  
+#5a. HINT: use tapply()
+  #unweighted difference in mean arrests
+    t1_arrper <- with(stations, 
+                      tapply(arrperswipe, 
+                             list("High Poverty" = highpov, "Predominantly Black" = nblack), 
+                             mean) )
+  
+  #weighted difference in mean arrests
+  #formula for the weighted mean = Σxw / Σw
+    t1_arrper_wtd <-
+      tapply(stations$arrperswipe * stations$swipes2016,
+             list(stations$highpov, stations$nblack), 
+             sum) / 
+      tapply(stations$swipes2016,
+             list(stations$highpov, stations$nblack), 
+             sum)  
+
+  #ok so arrest intensity is higher in high-pov stations that are majority black
+  #are there differences in poverty rates that could in part explain this association?
+    t1_povrt <- with(stations, 
+                     tapply(povrt_all_2016, 
+                            list("High Poverty" = highpov, "Predominantly Black" = nblack), 
+                            mean) )    
+    t1_arrper
+    t1_arrper_wtd
+    t1_povrt
+  
+  
+#5b.
+  #scatterplot by nblack
+    ggplot(stations, aes(x = povrt_all_2016, y = arrperswipe, color = nblack)) +
+      geom_point() +
+      #Modify legend title and text
+      scale_color_discrete(name = "Predominantly Black Station",
+                           labels=c("No", "Yes"),
+                           # Reverse Label Order
+                           guide = guide_legend(reverse=TRUE)) +
+      #Modify legend aesthetics (optional)
+      theme(legend.position = "bottom", 
+            legend.background = element_rect(color = "black", fill = "grey90", size = .2, linetype = "solid"), 
+            legend.direction = "horizontal",
+            legend.text = element_text(size = 8), 
+            legend.title = element_text(size = 8) )
+  
+  #add linear plot
+
+  
+  #add quadratic plot
+
+
+#5c.  
+  
+
+  
+#5d.
+  
+    
+## -----------------------------------------------------------------------------
+## 6. Similar to analysis above, examine relationship between arrest intensity & criminal complaints
+##  
+##  a. read in "nypd_criminalcomplaints_2016.csv"
+##      - this csv file shows # of criminal complaints for each subway station area
+##      - source: https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i
+##
+##  b. join stations data frame to subway station area crime data
+##      - join on st_id
+##      - exclude the stations with the 4 highest counts of criminal complaints
+##        - why? bc they are in very close proximity to Brooklyn transit policing HQ,
+##          and to Brooklyn criminal courts, so they don't face comparable
+##          neighborhood policing conditions
+##
+##  c. examine relationship between arrest intensity & criminal complaints 
+##      i. First, look at over all relationship (don't take nblack into account)
+##      ii. Then, allow relationship to vary by nblack
+##
+##  NOTE: you don't need to follow *all* the same steps in questions 3-5.
+##    for 6.c.i. and 6.c.ii:
+##     - focus on showing your preferred plots to inform the relationship, 
+##       along w/any additional data manipulation & evidence to support your decisions/interpretation/conclusions
+##     - you'll want to explore the data before arriving at your preferred plots, 
+##       DON'T show us everything you tried, just the analysis you settled on to support your answers
+## -----------------------------------------------------------------------------
+
+#6a.  
+
+    
+#6b.
+
+  
+#6c. 
+  
+
+  
+## -----------------------------------------------------------------------------
+## 7. interpret your findings with respect to subway fare evasion enforcement bias based on race
+##    - any additional analysis you'd like to explore with the data at hand?
+##    - are there any key limitations to the data/analysis affecting your
+##       ability to assess enforcement bias based on race?
+##    - any additional data you'd like to see that would strengthen your analysis/interpretation 
+##    - for this question, try to be specific and avoid vaguely worded concerns
+## -----------------------------------------------------------------------------
+  
+  
