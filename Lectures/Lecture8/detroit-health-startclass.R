6d63aee3690f7e5c73e1cb875338df5ff0487ee0
niyatimalhotra <75914049+niyatimalhotra@users.noreply.github.com>
2021-03-07 16:13:56 -0500

    Add Lecture 8 materials to repository

--- Lectures/Lecture8/detroit-health-startclass.R
+++ Lectures/Lecture8/detroit-health-startclass.R
@@ -14,6 +14,8 @@
 ## Last week: who is most affected by this problem?
 ##
 ## This week: what are the associated public health impacts?
+##            - last week we created a tract-month panel
+##            - the public health data is at the zipcode-month level
 
 
 ## ---------------------------
@@ -38,8 +40,13 @@ library(multiwayvcov)
 ## ---------------------------
 
 getwd()
-#setwd("..")
-#setwd("../data")
+
+#syntax for changing working directory 
+setwd("Data") #go down to the data folder
+getwd()
+
+setwd("..")   #go back up one level to where we started
+getwd()
 
 
 
@@ -47,9 +54,10 @@ getwd()
 ## 1. Zip-code: gather all input data 
 ## -----------------------------------------------------------------------------
 
-#A. Read in zipcode-level demographic data from 2015 American Community Survey
-  
-  input_acs_zip <- read.dta13("data/ACS_13_17_5YR_Zip.dta")
+#A. Get 1 observation for every zipcode for population, income, race variables
+
+  #Read in zipcode-level demographic data from 2015 American Community Survey
+    input_acs_zip <- read.dta13("data/ACS_13_17_5YR_Zip.dta")
   
   #get/rename variables
     acs_zip.clean <- input_acs_zip %>% 
@@ -60,39 +68,46 @@ getwd()
              share_black = per_race_black_alone_or_combo,
              med_inc = num_income_median) 
   
-  #inspect -- what is unit of observation?
+  #double-check the unit of observation
     dim(acs_zip.clean)
     head(acs_zip.clean)
   
+    
 
 #B. Read in and join public health data to demographic data (by zip code)
   
   #read in public health data
     input_hcup_zip <- read.dta13("data/hcup_total_&_viral.dta")
+    
+    #notes:
+      #total_obs: total hospitalizations
+      #viral_infect: hospitalizations related to viral infections
  
    #what is unit of observation?
     table(input_hcup_zip$year, input_hcup_zip$month)
   
+    
   #join public health data (input_hcup_zip) to demographic data (input_acs_zip)
   #Q: FILL IN THE CODE TO THE RIGHT OF THE ASSIGNMENT OPERATOR
-      #join to end up with zipcode-month observations
-      #also use mutate along with make_date to create a date variable(month_year)
+    #join to end up with zipcode-month observations 
+    #lesson w/join examples: https://hreplots.github.io/U6614/Lectures/Lecture4/Lecture4.1.html
+    #also use mutate along with make_date to create a date variable(month_year)
     joined_temp1 <- 
       
   
-  #inspect -- double-check unit of observation?
-    dim(joined_temp1)
-    head(joined_temp1)
-    table(joined_temp1$year, joined_temp1$month)
-    
+    #inspect -- double-check unit of observation
+      dim(joined_temp1)
+      head(joined_temp1)
+      table(joined_temp1$year, joined_temp1$month)
   
+    
 
 #C. Read in SI (service interruption) data and aggregate to zip code level
 
   #get service interruption data
   input_si <- read.dta13("data/si_1017_cleaned.dta")
   
-  #focus on key variables to identify period/location of every shutoff
+  #focus on key variables to identify month/tract of every shutoff
   #we'll eventually want to join to demographic data to get zip5-month_year obs
   si.clean <- input_si %>% 
     select(si_order_number, zip5, year, month) %>% 
@@ -105,32 +120,31 @@ getwd()
     mutate(month_year = make_date(year, month)) %>% 
     arrange(zip5, year, month) 
   
-  #Q: CHECK IF EVERY MONTH IS REPRESENTED IN THE SI DATA? 
+  #QUESTION: CHECK IF EVERY MONTH IS REPRESENTED IN THE SI DATA? 
       #IF NOT, HOW SHOULD WE TREAT MONTHS W/NO SHUTOFF DATA?
-
-    
+    table(si_zip_ym$year, si_zip_ym$month)
   
+  
+    
 #D. join zipcode-date level SI data (si_zip_ym) to joined ACS/health data (joined_temp1)
   
   #Q: WHAT KIND OF JOIN? DIFFERENT WAYS TO APPROACH THIS...
       #HINT: start by thinking about the unit of obs you want to end up with
   
-  #the catch is that missing SI data before 2018 is not missing but just 0's
+  #the catch here is that missing SI data before 2018 should be 0's not missing
   #one approach: a full_join w/joined_temp1 on the "left" bc it has every zip-date pair
     #so we have dates w/no SI data, we just have to fill in si_count as 0s
   joined_temp2 <- 
     
   
-  #inspect -- double-check NAs
-    summary(joined_temp2$si_count)
+  #inspect
     summary(joined_temp2)
     
-
   #generate quarter column for joining home vacancy data in next step
-  joined_temp3 <- joined_temp2 %>% 
-    mutate(quarter = 3 * as.numeric(cut(joined_temp2$month, 
-                                    breaks = c(0,3,6,9,12), 
-                                    labels = FALSE ) ) )
+    joined_temp3 <- joined_temp2 %>% 
+      mutate(quarter = 3 * as.numeric(cut(joined_temp2$month, 
+                                      breaks = c(0,3,6,9,12), 
+                                      labels = FALSE ) ) )
 
   
 #E. Read in vacancy data and join to previously joined data (joined_temp3)
@@ -145,11 +159,12 @@ getwd()
     table(input_vacancy_qtr$zip5, input_vacancy_qtr$year)
     joined_temp4 <- 
       
-
-  #inspect
-    dim(joined_temp4)
-    head(joined_temp4)
+      
+    #inspect
+      dim(joined_temp4)
+      head(joined_temp4)
   
+    
 
 #F. Panel data: transform input vars to rates and get IDs for panel data
   
@@ -162,17 +177,19 @@ getwd()
            zip5_fac = as.factor(zip5),
            ym_fac = as.factor(ym)) 
   
+  
   #get FE dummy for each year-month combination, and for each zipcode
     zip_panel <- dummy_cols(zip_panel, select_columns = c("ym", "zip5")) 
 
   
   #remove temporary data frames from environment (check the upper right pane)
-  rm(joined_temp1, joined_temp2, joined_temp3)
+    rm(joined_temp1, joined_temp2, joined_temp3)
+  
   
   
 #G. Also collapse panel data to one observation for each zip code
     #eliminates variation within zip codes over time
-    #left with between-zip code variation only
+    #left with between-zip code variation only ("cross-sectional" data)
   
   zip_cross <- joined_temp4 %>% 
     group_by(zip5) %>% 
@@ -186,7 +203,11 @@ getwd()
            si_1000 = (si_count / pop2015) * 1000,
            viral_infect_1000 = (viral_infect / pop2015) * 1000,
            vac_res_p100 = (vac_res / total_res) * 100 )
-
+  
+  #sample code if you want to save this as a dataframe or csv
+    save(zip_cross, file="data/zip_cross.rda")
+    write.csv(zip_cross, "data/zip_cross.csv")
+            
   rm(joined_temp4)
   
 
@@ -195,17 +216,19 @@ getwd()
 ## 2. Cross-sectional: analyze health outcomes vs service interruptions
 ## -----------------------------------------------------------------------------
 
-#total hospital admissions as dependent variable  
+#total hospital admissions as dependent variable (with + without vacancy control)
   cross_total_1 <- lm(total_obs_1000 ~ si_1000, 
                       data = zip_cross, weight = pop2015)
   summary(cross_total_1)
+  coeftest(cross_total_1, vcov = vcovHC(cross_total_1, type="HC1")) #robust SEs
+  
   #QUESTION: Write out the PRF and interpret coefficient
   
   
   cross_total_2 <- lm(total_obs_1000 ~ si_1000 + vac_res_p100, 
                       data = zip_cross, weight = pop2015)
   summary(cross_total_2)
-
+  coeftest(cross_total_2, vcov = vcovHC(cross_total_2, type="HC1")) #robust SEs
   
 
 #plot cross-sectional data: shutoff rate vs. hospital admissions (weight by pop2015)
@@ -215,9 +238,11 @@ getwd()
     scale_size(range = c(0.1, 6), guide = "none") +
     geom_smooth(method = 'lm', formula = y ~ x)
 
-  #check correlation (unweighted)
+  #check correlation
   cor(zip_cross$si_1000, zip_cross$vac_res_p100, use = "pairwise.complete.obs")
   wtd.cor(zip_cross$si_1000, zip_cross$vac_res_p100, weight = zip_cross$pop2015)
+  
+  #QUESTION: what should we make of this association? think about internal validity.
   
   
 
@@ -228,15 +253,15 @@ getwd()
 #estimation with FEs for zip5 (entity) and ym (time) [NO VACANCY RATE CONTROL]
 
   #Approach A: LSDV model
-  
-    #QUESTION: FILL IN MODEL FORMULA
     
-    panel_total_1 <- lm(FORMULA GOES HERE, 
-                        data = zip_panel, 
-                        weight = pop2015)
+    #QUESTION: FILL IN MODEL FORMULA
+    panel_total_1 <- lm(FILL IN CODE HERE)
+  
     summary(panel_total_1)
     panel_total_1$coefficients[1:2]
     summary(panel_total_1)$adj.r.squared
+    
+    #QUESTION: what would happen if we didn't coerce zip5 into a factor?
     
     #robust SEs
       coeftest(panel_total_1, vcov = vcovHC(panel_total_1, type = "HC1"))[2,]
@@ -260,7 +285,7 @@ getwd()
                         model = "within",
                         weight = pop2015 )
     summary(panel_total_1b)
-      #note 1: R-square calculations are off
+      #note 1: here the R-squared stat is not for the full model!
       #note 2: can't estimate robust SEs using weighted plm models
     
     #QUESTION: Write out the PRF and interpret coefficient
@@ -268,33 +293,37 @@ getwd()
   
 
 #estimation with FEs for zip5 (entity) and ym (time) + control for vacancy rate
- #Approach A: LSDV model
-  #self-test: try replicating results w/Approach B (FE estimation w/plm package)
+  
+  #Approach A: LSDV model
+    #try replicating this with Approach B (FE estimation w/plm package)
     
-  panel_total_2 <- lm(total_obs_1000 ~ si_1000 + vac_res_p100 + as.factor(zip5) + as.factor(ym),
+    panel_total_2 <- lm(total_obs_1000 ~ si_1000 + vac_res_p100 + as.factor(zip5) + as.factor(ym),
                       data = zip_panel, 
                       weight = pop2015)
-  summary(panel_total_2)
-  summary(panel_total_2)$adj.r.squared
-  coeftest(panel_total_2, vcov = vcovHC(panel_total_2, type = "HC1"))[2,]
+    summary(panel_total_2)
+    summary(panel_total_2)$adj.r.squared
+    coeftest(panel_total_2, vcov = vcovHC(panel_total_2, type = "HC1"))[2,]
   
-  #clustered SEs by zip code (equivalent to areg in Stata)
-    #stats for coefficient of interest is the 2nd element in this object. 
-    panel_total_2_vcov <- cluster.vcov(panel_total_2, 
-                                       cbind(zip_panel$zip5),
-                                       df_correction = T) #a small sample size adjustment
-    
-    #just report stats for coefficient of interest (the second row)
-    coeftest(panel_total_2, panel_total_2_vcov)[2,] 
+    #clustered SEs by zip code (equivalent to areg w/cluster option in Stata)
+      #stats for coefficient of interest is the 2nd element in this object. 
+      panel_total_2_vcov <- cluster.vcov(panel_total_2, 
+                                         cbind(zip_panel$zip5),
+                                         df_correction = T) #a small sample size adjustment
+      
+      #just report stats for coefficient of interest (the second row)
+      coeftest(panel_total_2, panel_total_2_vcov)[2,] 
 
     
     
-#bonus plots: plot model results 
+#plot model results 
    
   #plot relationship using pooled cross-sectional data
     ggplot(zip_panel, aes(x = si_1000, y = total_obs_1000, weight = pop2015)) +
       geom_point() +
       geom_smooth(method = 'lm', formula = y ~ x, se = FALSE)
+    
+    #QUESTION: why doesn't this plot correspond to our FEs estimates?
+    
     
     #can also plot binned data
     ggplot(zip_panel, aes(x = si_1000, y = total_obs_1000, weight = pop2015)) +
@@ -303,21 +332,23 @@ getwd()
                        color = 'orange', size=3, geom = 'point') +
       geom_smooth(method = 'lm', formula = y ~ x, se = FALSE)
     
+      #QUESTION: why do you think this approach could be considered misleading?
+    
     
   #plot FE results by plotting residual shutoff rate vs residual hospital admission rate 
       #(residuals after accounting for FEs)
+    #if you're confused here, review Lecture 7 & Quant II Video Lecture 5.2.a on FEs
+    
     panel_total_y_feonly <- lm(total_obs_1000 ~ as.factor(zip5) + as.factor(ym), 
                             data = zip_panel, 
                             weight = pop2015)
     panel_total_x_feonly <- lm(si_1000 ~ as.factor(zip5) + as.factor(ym), 
                                data = zip_panel, 
                                weight = pop2015)
-    
+
     ggplot(data = zip_panel, aes(x= panel_total_x_feonly$residuals, 
                                  y = panel_total_y_feonly$residuals, 
                                  weight = pop2015)) +
       geom_point(alpha = 0.2) +
       geom_smooth(method = 'lm', formula = y ~ x, se = FALSE)
 
-  
-  
