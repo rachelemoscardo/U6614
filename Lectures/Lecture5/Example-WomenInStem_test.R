85aec73631d4e59322c853a3a8af9777cccdc9c1
ltk2118 <liam.k@columbia.edu>
2022-02-19 03:34:04 -0500

    update women in stem

--- Lectures/Lecture5/Example-WomenInStem_test.R
+++ Lectures/Lecture5/Example-WomenInStem_test.R
@@ -21,9 +21,8 @@
 ## load libraries
 ##-------------------------
 
-
-#install.packages("ggrepel")
-#install.package("ggpmisc")
+# install.packages("ggrepel")
+#install.packages("ggpmisc")
 
 library(tidyverse)
 library(ggrepel)
@@ -38,10 +37,9 @@ getwd()
 
 wbgender <- read_csv("worldbank-genderstats.csv", na = "..")
 
-#wait this data isn't "tidy"! 
-#each row is not its own observation! there are diff rows for diff vars
+#wait this data isn't "tidy"! diff vars for diff variables is... weird
   
-#here are the 3 input variables we want to work with
+#first, let's keep *rows* with information for 3 variables:
   #SE.TER.GRAD.FE.SI.ZS : 
     #Y: Female share of graduates from STEM programmes, tertiary (%)
   #SH.MMR.LEVE :
@@ -49,12 +47,9 @@ wbgender <- read_csv("worldbank-genderstats.csv", na = "..")
   #SP.UWT.TFRT :
     #X2: Unmet need for contraception (% of married women ages 15-49) 
 
-
-#first, let's keep *rows* with information for 3 variables:
   keepvars <- c("SE.TER.GRAD.FE.SI.ZS", "SH.MMR.LEVE", "SP.UWT.TFRT")
   stem <- wbgender %>% filter(`Series Code` %in% keepvars)
-    #NOTE: use backticks to refer to `Series Code` bc of space in col name 
-    #NOTE: the %in% operator checks if two vectors contain overlapping values 
+    #note: use backticks to refer to `Series Code` bc of space in col name 
 
   
 ##---------------------------
@@ -64,18 +59,14 @@ wbgender <- read_csv("worldbank-genderstats.csv", na = "..")
 ## A. create 3 separate df's for each variable
 ##    - stem.fmshstemgrads, stem.dayspaidmatleave, stem.unmetcontr
 ##    - each df should be a subset of rows for each variable 
-##    - look at the distribution of each variable in each year - any concerns?
   
   stem.fmshstemgrads <- wbgender %>% filter(`Series Code` == "SE.TER.GRAD.FE.SI.ZS")
   stem.dayspaidmatleave <- wbgender %>% filter(`Series Code` == "SH.MMR.LEVE")
   stem.unmetcontr <- wbgender %>% filter(`Series Code` == "SP.UWT.TFRT")
-  
   summary(stem.fmshstemgrads)
-  summary(stem.dayspaidmatleave)
-  summary(stem.unmetcontr)
     #uh oh, way too many missing values in any given year!
     #obs come from diff surveys in diff years. values of vars may change slowly.
-    #for now let's try using the maximum over all years to reduce NAs
+    #let's try using the maximum over all years to reduce NAs
       #i.e. get max fem share of STEM grads for every year (2011-2020)
   
   
@@ -83,47 +74,47 @@ wbgender <- read_csv("worldbank-genderstats.csv", na = "..")
 ## B. create 'analysis variables' = max value across all years for each country
 ##    - hint: use mutate to create a new column
 ##    - hint: with mutate, use the pmax() function to get the max across rows
-##    - pay attention to how NAs are treated
+##    - pay attention to NAs
 ##    - only keep new cols for each analysis var, `Country Name`, `Country Code`
   
   stem.fmshstemgrads <- stem.fmshstemgrads %>% 
-    mutate(fmshstemgrads = pmax(`2011 [YR2011]`, `2012 [YR2012]`, `2013 [YR2013]`, 
-                                `2014 [YR2014]`, `2015 [YR2015]`, `2016 [YR2016]`,
-                                `2017 [YR2017]`, `2018 [YR2018]`, `2019 [YR2019]`, na.rm = TRUE)) %>% 
-    select(`Country Name`, `Country Code`, fmshstemgrads) 
-
+    pivot_longer(cols = `2011 [YR2011]`:`2020 [YR2020]`,
+                 names_to = "Year",
+                 values_to = "female_share") %>% 
+    group_by(`Country Name`,`Country Code`) %>% 
+    summarise(fmshstemgrads = max(female_share, na.rm=TRUE))
+  
+  stem.dayspaidmatleave  <- stem.dayspaidmatleave  %>% 
+    pivot_longer(cols = `2011 [YR2011]`:`2020 [YR2020]`,
+                 names_to = "Year",
+                 values_to = "female_share") %>% 
+    group_by(`Country Name`,`Country Code`) %>% 
+    summarise(dayspaidmatleave = max(female_share, na.rm=TRUE))
+  
   stem.unmetcontr <- stem.unmetcontr %>% 
-    mutate(unmetcontr = pmax(`2011 [YR2011]`, `2012 [YR2012]`, `2013 [YR2013]`, 
-                                `2014 [YR2014]`, `2015 [YR2015]`, `2016 [YR2016]`,
-                                `2017 [YR2017]`, `2018 [YR2018]`, `2019 [YR2019]`, na.rm = TRUE)) %>% 
-    select(`Country Name`, `Country Code`, unmetcontr) 
-  
-  stem.dayspaidmatleave <- stem.dayspaidmatleave %>% 
-    mutate(dayspaidmatleave = pmax(`2011 [YR2011]`, `2012 [YR2012]`, `2013 [YR2013]`, 
-                                `2014 [YR2014]`, `2015 [YR2015]`, `2016 [YR2016]`,
-                                `2017 [YR2017]`, `2018 [YR2018]`, `2019 [YR2019]`, na.rm = TRUE)) %>% 
-    select(`Country Name`, `Country Code`, dayspaidmatleave) 
+    pivot_longer(cols = `2011 [YR2011]`:`2020 [YR2020]`,
+                 names_to = "Year",
+                 values_to = "female_share") %>% 
+    group_by(`Country Name`,`Country Code`) %>% 
+    summarise(unmetcontr = max(female_share, na.rm=TRUE))
 
   
-  #here's an alternative solution that avoids writing out every column name
-  stem.fmshstemgrads <- wbgender %>% 
-    filter(`Series Code` == "SE.TER.GRAD.FE.SI.ZS") 
-  stem.fmshstemgrads <- stem.fmshstemgrads %>%
-    mutate(fmshstemgrads = do.call(pmax, c(stem.fmshstemgrads[,5:14], na.rm=TRUE))) %>% 
-    select(`Country Name`, `Country Code`, fmshstemgrads)  
+  #here's an alternative solution that avoids hardcoding column names
+  stem.fmshstemgrads <- stem.fmshstemgrads %>% 
+    mutate(fmshstemgrads = do.call(pmax, c(stem.fmshstemgrads[,5:14], na.rm=TRUE)))
   
   
   
 ## C. join 3 new df's together to get a single "tidy" dataframe
-##    - include 3 analysis variables and `Country Name` and `Country Code`
-##    - how many countries have non-missing values for all 3 vars?
+##    - including 3 analysis variables and `Country Name` and `Country Code`
 
-  stem.cross <- inner_join(stem.fmshstemgrads, stem.unmetcontr) %>% 
+  stem.cross <- stem.fmshstemgrads %>% 
+    inner_join(stem.unmetcontr) %>% 
     inner_join(stem.dayspaidmatleave) %>% 
     na.omit()
 
   #think about sample selection issues! 
-  #are missing observations for original variables randomly distributed?
+  #are missing observations for 3 analysis variables randomly distributed?
 
     
   
@@ -131,17 +122,10 @@ wbgender <- read_csv("worldbank-genderstats.csv", na = "..")
 ## 3. exploratory analysis
 ##---------------------------
 
-## stem.cross has issues w/measurement error and non-random sample selection
-## these issues limit the usefulness of this data for credible project work
-## but we'll proceed a bit further for this in-class exercise 
 
-  
 ## A. explore relationship between days paid maternity leave & fem share of STEM grads
     ggplot(stem.cross, 
-           aes(x = dayspaidmatleave, 
-               y = fmshstemgrads, 
-               label = `Country Code`, 
-               color = unmetcontr)) + 
+           aes(x = dayspaidmatleave, y = fmshstemgrads, label = `Country Code`, color = unmetcontr)) + 
       geom_point() + 
       stat_dens2d_filter(geom = "text_repel", keep.fraction = 0.2) +
       scale_colour_gradient(low = "blue", high = "red") +
@@ -159,8 +143,6 @@ wbgender <- read_csv("worldbank-genderstats.csv", na = "..")
 
     
 ## B. could other country-level differences in part explain this relationship?
-##    - what can you check in the data? 
-    
     cor(stem.cross$dayspaidmatleave, stem.cross$unmetcontr)
     
     #there surely are! this is the usual problem w/cross-sectional variation
