5148bb6bef44b267551a146282eed40c6de4c710
ltk2118 <liam.k@columbia.edu>
2022-09-27 15:57:09 -0400

    Update lecture 4 inclass.R

--- Lectures-gh/Lecture4-gh/Lecture4-inclass.R
+++ Lectures-gh/Lecture4-gh/Lecture4-inclass.R
@@ -33,30 +33,36 @@ getwd()
 ## -----------------------------------------------------------------------------
 ## 2. Aggregating to subway station-level arrest totals
 ##
-##  a. load cleaned/appended arrest microdata (arrests_all.csv) w/all strings as factors
+##  a. load full set of cleaned arrest microdata (arrests.clean.rdata)
 ##
 ##  b. aggregate microdata to station-level observations w/the following info:
 ##      - st_id, loc2, arrests_all (=arrest count)
 ##      - store results as new data frame st_arrests
+##      - inspect new data frame (but don't include inspection code in submission)
 ##
 ##  c. plot histogram of arrests and briefly describe distribution of arrests across stations
 ## -----------------------------------------------------------------------------
 
 #2a.
-  arrests_all <- read.csv("arrests_all.csv", 
-                         stringsAsFactors = TRUE,
-                          na.strings = c("")) #specify string values to read in as NA (here blanks)
+load("arrests.clean.RData")
+
+
 #2b.
-  st_arrests <- arrests_all %>% 
+  st_arrests <- arrests.clean %>% 
     group_by(st_id, loc2) %>% 
     summarise(arrests_all = n() ) %>% 
     arrange(desc(arrests_all))
 
-  #could use count() in place of group_by() & summarise() - shorter but less flexible
-  arrests_all %>% 
+  #could use count() instead of group_by()+summarise() - shorter but less flexible
+  arrests.clean %>% 
     count(st_id, loc2) %>% 
     arrange(desc(n))
   
+  #inspect - DO NOT INCLUDE IN RMD SUBMISSION
+  str(st_arrests)  #why is this so long?
+  str(st_arrests, give.attr = FALSE)
+    #can also consider ungrouping the st_arrests dataframe 
+
 #2c.
   ggplot(data = st_arrests, aes(x = arrests_all)) + geom_histogram()
   
@@ -67,10 +73,10 @@ getwd()
 ## -----------------------------------------------------------------------------
 ## 3. joining ridership and neighborhood demographics to arrest data
 ##
-##  a. import other station-level datasets and inspect
+##  a. read in other station-level csv files (w/strings as factors) and inspect
 ##      - don't include inspection code in your rmd submission
 ##
-##  b. join both files to st_arrests and inspect results (store new df as st_joined),
+##  b. join both data frames to st_arrests & inspect results (store new df as st_joined)
 ##      - inspect results of join and describe any issues
 ##      - drop unnecessary columns from the ridership data
 ##      - group st_joined by st_id and mta_name
@@ -106,22 +112,44 @@ getwd()
 
 
 #3b.
-  #a vector of columns we don't need to keep
-    drop_vars <- c("swipes2011", "swipes2012", "swipes2013", "swipes2014", "swipes2015")
+  
+  #a vector of columns we don't need to keep, will use in 3b
+  drop_vars <- c("swipes2011", "swipes2012", "swipes2013", "swipes2014", "swipes2015")
+  
+  #example: join st_arrests to st_poverty
+  st_joinedtemp <- inner_join(st_arrests, st_poverty, by = c("st_id" = "st_id"))
+    #uh-oh, doesn't work!
+  
+  #what's the problem? st_id is a factor in st_arrests but an integer in st_poverty!
+  #need to join on columns of the same data type
+  st_arrests <- st_arrests %>% 
+    mutate(st_id = as.integer(st_id))
+  st_joinedtemp <- inner_join(st_arrests, st_poverty, by = c("st_id" = "st_id"))
+  
+  rm(st_joinedtemp)
+  
     
   #in-class exercise: join all 3 data frames (in a single pipe if you can):
-    #3 data frames to join: st_poverty, st_ridership, st_arrests
-  st_joined <- st_poverty %>%
-    inner_join(st_ridership) %>%
-    inner_join(st_arrests) %>% 
-    select(!drop_vars) %>% 
+    #3 data frames to join: st_arrests, st_poverty, st_ridership
+  st_joined <- st_arrests %>%
+    inner_join(st_poverty, by = c("st_id")) %>%
+    inner_join(st_ridership, by = c("st_id" = "st_id",
+                                    "mta_name" = "mta_name")) %>% 
+    select(-all_of(drop_vars)) %>% 
     group_by(st_id, mta_name) 
 
+    #why use all_of()? 
+    #https://stackoverflow.com/questions/62397267/why-should-i-use-all-of-to-select-columns
+  
   #inspect - DO NOT INCLUDE IN RMD SUBMISSION
-    str(st_joined)  #why is this so long?
+    #display structure of ungrouped data frame to avoid lengthy output listing every group
+    st_joined %>% ungroup() %>% str(give.attr = FALSE)
     summary(st_joined)
       #Note: 157 obs in joined df w/no NAs (except some missing demographics) 
       #inner join worked as intended!
+    
+  #QUESTION: could we have used full_join or left_join here instead of inner_join?
+    #yes! in this case all 3 datasets have been pre-cleaned to have 157 obs.
 
     
 #3c.
@@ -159,7 +187,7 @@ getwd()
 
 #4a.
   stations <- st_joined %>%
-    mutate(arrperswipe = round(arrests_all / (swipes2016 / 100000), 2),
+    mutate(arrperswipe = round(arrests_all / (swipes2016 / 100000), 2), #arrest intensity
            highpov = as.numeric(povrt_all_2016 > median(st_joined$povrt_all_2016)),
            nblack = as.numeric(shareblack > .5),
            shareblack = round(shareblack, 2),
@@ -189,17 +217,18 @@ getwd()
    stations %>% 
      arrange(desc(arrperswipe)) %>% 
      select(st_id, mta_name, arrperswipe, arrests_all, shareblack, povrt_all_2016, highpov, nblack) %>% 
-     head(n = 10) %>% 
-     kable() #kable offers better table formatting
+     head(n = 10) %>% view() 
+     #kable() #kable offers better table formatting
   
 
   
 #4b. 
-  ggplot(stations, #specify dataframe to use
+  ggplot(data = stations, #specify dataframe to use
          aes(x = povrt_all_2016, y = arrperswipe)) + #specify columns to use
-    geom_point() + #specify plot geometry
-    ggtitle('Scatterplot of arrest rate vs. poverty rate') + #add title
-    labs(x = 'poverty rate', y = 'arrest rate') #change axis labels
+    geom_point(aes(size=pop_all_2016)) + 
+    geom_smooth() #specify plot geometry
+    #('Scatterplot of arrest rate vs. poverty rate') + #add title
+    #labs(x = 'poverty rate', y = 'arrest rate') #change axis labels
 
   #fit linear model with station observations (can also add optional weights argument)
     ols1l <- lm(arrperswipe ~ povrt_all_2016, data = stations)
@@ -207,6 +236,12 @@ getwd()
     coeftest(ols1l, vcov = vcovHC(ols1l, type="HC1")) #get robust SEs
   
   #how to refer to specific regression results
+    summary_ols1l <- summary(ols1l)
+    summary_ols1l$adj.r.squared
+    
+    summary_ols1l$residuals
+   
+    
     ?summary.lm
     summary(ols1l)$adj.r.squared  #adj R-square
     summary(ols1l)$coefficients   #coefficients
@@ -219,7 +254,8 @@ getwd()
       geom_point() + 
       ggtitle('Scatterplot of arrest rate vs. poverty rate') + 
       labs(x = 'poverty rate', y = 'arrest rate') + 
-      ADD GEOMETRY FOR REGRESSION LINE #add linear SRF
+      geom_smooth(method = "lm", formula = y~x) + #add linear SRF
+      geom_smooth(method = "lm", formula = y~x  + I(x^2), color="red")
 
   #fit quadratic OLS model (arrest rate vs. poverty rate)
     #HINT: see quadratic syntax from Lecture4.2 (section 4.1)
@@ -238,11 +274,19 @@ getwd()
 
   
 #4c. calculate and test difference in means between high/low poverty stations
-    
+    stations %>% 
+      ungroup() %>% 
+      group_by(highpov) %>% 
+      summarise(n = n(),
+                mean_pov = mean(povrt_all_2016),
+                mean_arrper = mean(arrperswipe))
 
+t.test(arrperswipe ~ highpov, data = stations)
 
-    
+diff_in_means <- lm(arrperswipe ~ highpov, data = stations, weights = swipes2016)
+ summary(diff_in_means)
 
+ 
 ## -----------------------------------------------------------------------------
 ## 5. How does neighborhood racial composition mediate the relationship between poverty and arrest intensity
 ##    - examine relationship between arrest intensity & poverty by Black vs non-Black station area (nblack)
